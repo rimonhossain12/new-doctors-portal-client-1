30bcac65b6d157c44f952d962cc7c9396c317aec
rimonspi12 <rimonmpi@gmail.com>
2022-10-25 18:23:41 +0600

    dynamically added information

--- src/Pages/Home/Information/InfoCard.js
+++ src/Pages/Home/Information/InfoCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import chair from '../../../assets/images/chair.png';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
-            <figure><img src={img} alt="Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
+        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+            <figure className="pl-5"><img src={img} alt="Album" /></figure>
+            <div class="card-body text-white">
+                <h2 class="card-title">{cardTitle}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
             </div>
         </div>
