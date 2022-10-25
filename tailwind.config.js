689e7e083c56f9a9fa255a0fb06d2cad5cba3247
rimonspi12 <rimonmpi@gmail.com>
2022-10-25 16:51:41 +0600

    doctors-portal-client-1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AC",
+          accent: "#3AA256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
