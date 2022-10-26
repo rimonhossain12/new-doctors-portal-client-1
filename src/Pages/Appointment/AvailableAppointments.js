b9954781df9791835f149d51d521b0760db40fa0
rimonspi12 <rimonmpi@gmail.com>
2022-10-26 16:27:53 +0600

    show all the datafrom local computer

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import format from 'date-fns/format';
+import { useState, useEffect } from 'react';
+import Service from './Service';
+
 
 const AvailableAppointments = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            Available Appointments on {format(date, 'PP')}
-
+            <h4 className='text-xl text-secondary text-center hover:underline'>Available Appointments on {format(date, 'PP')}</h4>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-12'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    />)
+                }
+            </div>
         </div>
     );
 };
