69585b82ba9aef45a396e1d7144a76e41b82972f
rimonspi12 <rimonmpi@gmail.com>
2022-10-26 17:38:39 +0600

    close modal button from parents elements

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -27,7 +27,10 @@ const AvailableAppointments = ({ date }) => {
                     />)
                 }
             </div>
-            {treatment && <BookingModal date={date} treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal
+                date={date} treatment={treatment}
+                setTreatment={setTreatment}
+            ></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import format from 'date-fns/format';
 
-const BookingModal = ({ treatment, date }) => {
-    const { name, slots } = treatment;
+const BookingModal = ({ treatment, date, setTreatment }) => {
+    const { _id, name, slots } = treatment;
+    const handleBooking = (event) => {
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(_id, slot, name);
+        setTreatment();
+    }
     return (
         <div>
             {/* Put this part before </body> tag */}
@@ -11,16 +17,16 @@ const BookingModal = ({ treatment, date }) => {
                 <div className="modal-box">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-secondary">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 justify-items-center mt-4'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-4'>
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name='slot' className="select select-bordered w-full max-w-xs">
                             {
                                 slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
                     {/* <div className="modal-action">
