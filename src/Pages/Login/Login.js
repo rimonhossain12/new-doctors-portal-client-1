c2b0dccc8c9ea4a47818ab77eee6dadee7d44518
rimonspi12 <rimonmpi@gmail.com>
2022-11-04 11:59:09 +0600

    jwt token found backend

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,11 +4,13 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+
     const [
         signInWithEmailAndPassword,
         user,
@@ -16,17 +18,18 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    const [token] = useToken(gUser || user)
     let signInError;
     const navigate = useNavigate();
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
 
-    useEffect(() => {       
-        if (user || gUser) {
+    useEffect(() => {
+        if (token) {
             navigate(from, { replace: true });
         }
-    }, [user, gUser, from, navigate])
+    }, [token, from, navigate])
 
 
 
